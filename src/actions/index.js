8c4a4383a022c26228f8edcf934b71bcbb550b68
Ekdanai Deeboonchai <ekdanai.d@gmail.com>
2019-04-25 10:07:42 +0700

    [Update] actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,32 @@
-export const actionA = () => {
+export const updateUser = (name, lname) => {
   return function(dispatch) {
     dispatch({
-      type: 'USER_SELECT',
-      payload: 'got it from Action A'
+      type: 'UPDATE_USER',
+      payload: { name: name, lname: lname }
+    });
+  };
+};
+export const createNewUser = (name, lname) => {
+  return function(dispatch) {
+    dispatch({
+      type: 'CREATE_NEW_USER',
+      payload: { name: name, lname: lname }
+    });
+  };
+};
+export const removeUser = id => {
+  return function(dispatch) {
+    dispatch({
+      type: 'REMOVE_USER',
+      payload: { id: id }
+    });
+  };
+};
+export const selectUser = user => {
+  return function(dispatch) {
+    dispatch({
+      type: 'SELECT_USER',
+      payload: { user }
     });
   };
 };
